fbf73eef1eef65ee9d8b8ae4d516fc9ee135201f
Berkay <Berkay_Sebat@yahoo.com>
2016-05-26 23:44:39 -0400

    Initial Commit

---
+++ IOSProgrammerTest/Main Menu/MainMenuViewController.m
@@ -0,0 +1,50 @@
+//
+//  ViewController.m
+//  IOSProgrammerTest
+//
+//  Created by Justin LeClair on 12/15/14.
+//  Copyright (c) 2014 AppPartner. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+#import "ChatSectionViewController.h"
+#import "LoginSectionViewController.h"
+#import "AnimationSectionViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    UIImageView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bg_main_menu"]];
+    backgroundView.frame = self.view.bounds;
+    [[self view] addSubview:backgroundView];
+     [self.view insertSubview:backgroundView atIndex:0];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)tableSectionAction:(id)sender
+{
+    ChatSectionViewController *tableSectionViewController = [[ChatSectionViewController alloc] init];
+    [self.navigationController pushViewController:tableSectionViewController animated:YES];
+}
+- (IBAction)apiSectionAction:(id)sender
+{
+    LoginSectionViewController *apiSectionViewController = [[LoginSectionViewController alloc] init];
+    [self.navigationController pushViewController:apiSectionViewController animated:YES];
+}
+- (IBAction)animationSectionAction:(id)sender
+{
+    AnimationSectionViewController *animationSectionViewController = [[AnimationSectionViewController alloc] init];
+    [self.navigationController pushViewController:animationSectionViewController animated:YES];
+}
+@end
