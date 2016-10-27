ed6cf9ecbaee1fe3ea238296fadd62714f764ef3
Berkay <Berkay_Sebat@yahoo.com>
2016-10-27 09:36:44 -0400

    Added two new view controllers

---
+++ Nuevo.h
@@ -0,0 +1,13 @@
+//
+//  Nuevo.h
+//  NoStoryboard
+//
+//  Created by macbook pro on 10/27/16.
+//  Copyright © 2016 berkaysebat.com.Practice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Nuevo : UIView
+
+@end
---
+++ Nuevo.m
@@ -0,0 +1,21 @@
+//
+//  Nuevo.m
+//  NoStoryboard
+//
+//  Created by macbook pro on 10/27/16.
+//  Copyright © 2016 berkaysebat.com.Practice. All rights reserved.
+//
+
+#import "Nuevo.h"
+
+@implementation Nuevo
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
