2a858fbe917d3eaf7bc6163dbac6133a4db531ca
Berkay <Berkay_Sebat@yahoo.com>
2016-10-26 20:08:56 -0400

    added new classes

---
+++ NewClass.h
@@ -0,0 +1,13 @@
+//
+//  NewClass.h
+//  NoStoryboard
+//
+//  Created by macbook pro on 10/26/16.
+//  Copyright © 2016 berkaysebat.com.Practice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewClass : UIView
+
+@end
---
+++ NewClass.m
@@ -0,0 +1,21 @@
+//
+//  NewClass.m
+//  NoStoryboard
+//
+//  Created by macbook pro on 10/26/16.
+//  Copyright © 2016 berkaysebat.com.Practice. All rights reserved.
+//
+
+#import "NewClass.h"
+
+@implementation NewClass
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
