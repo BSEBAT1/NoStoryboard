b0e8d88ad1fb6e18abdd20c618c7a34fbd1559f6
Berkay <Berkay_Sebat@yahoo.com>
2016-11-10 08:59:11 -0600

    changes

--- NoStoryboard/ViewController.m
+++ NoStoryboard/ViewController.m
@@ -109,7 +109,7 @@ NSDictionary *viewsDictionary = @{@"redView":self.table,@"dotview":self.dot};
 -(NSString *)demo{
     return _demo;
 }
-///changed random///
+///THIS IS THE MASTER BRANCH//
 -(void)setDemo:(NSString *)newdemo{
      _demo=[newdemo stringByAppendingString:@"you created another instance"];
    
